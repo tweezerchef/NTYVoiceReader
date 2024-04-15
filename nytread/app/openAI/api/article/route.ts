fa165a0a9ff7fdf990281bcfea55102bd82244ff
tweezerchef <ltomblock@gmail.com>
2024-04-15 16:56:45 -0500

    getting text of article, converting to audio, need to handle client side return

--- nytread/app/openAI/api/article/route.ts
+++ nytread/app/openAI/api/article/route.ts
@@ -4,6 +4,7 @@ import { openAIAudioToText } from '../../../../utility/openAIAudioToText'
 import { openAITextToAudio } from '../../../../utility/openAITextToAudio';
 import 'dotenv/config';
 import leven from 'leven';
+import { nyTimesArticleParser } from '../../../../utility/articleScraper';
 
 interface Article {
   index: number;
@@ -15,19 +16,6 @@ interface Article {
 
 const selections = ['home', 'zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve'];
 
-// function getClosestArticle(transcription: string): string {
-//   let closestMatch = '';
-//   let smallestDistance = Infinity;
-
-//   for(let selection of selections) {
-//     const distance = leven(transcription.toLocaleLowerCase(), selection);
-//     if (distance < smallestDistance){
-//       smallestDistance = distance;
-//       closestMatch = selection;
-//     }
-//   }
-// return closestMatch;
-// }
 async function getClosestArticleIndex(transcription: string): Promise<number> {
   let closestMatchIndex = -1;
   let smallestDistance = Infinity;
@@ -69,26 +57,11 @@ export const POST = async (req: Request): Promise<Response> => {
     const selectedArticleURL = articles[articleIndex].url;
     console.log(selectedArticleURL);
 
-    // const response = await fetch(`https://api.nytimes.com/svc/topstories/v2/${closestSection}.json?api-key=${NYTAPIKEY}`);
-    // const data = await response.json();
-
-    // const articles = await Promise.all(data.results.map(async (article: any, index: number) => {
-    //   const audioBase64 = await openAITextToAudio(`Article Number ${index}, ${article.title} by ${article.byline}. ${article.abstract}`);
-    //   return {
-    //     index: index,
-    //     title: article.title,
-    //     abstract: article.abstract,
-    //     byline: article.byline,
-    //     audio: audioBase64,
-    //     url: article.url
-    //   };
-    // }));
-
-    // const textsForVoice = articles.map((article: Article) => `${article.index}. ${article.title} by ${article.byline}. ${article.abstract}`);
-    // console.log(textsForVoice);
-    // return new Response(JSON.stringify(articles), {
-    //   headers: { 'Content-Type': 'application/json' }
-    // });
+    const articleText = await nyTimesArticleParser(selectedArticleURL);
+    const articleAudio = await openAITextToAudio(articleText);
+    return new Response(JSON.stringify({ audio: articleAudio }), {
+      headers: { 'Content-Type': 'application/json' }
+    });
   } catch (error) {
     console.error('Error in POST handler:', error);
     return new Response(JSON.stringify({ error: 'An error occurred' }), {
