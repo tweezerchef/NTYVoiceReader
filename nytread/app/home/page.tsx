cc4e5230e4dca9f662cb0be4a8fdc9e62ba6248b
tweezerchef <ltomblock@gmail.com>
2024-04-25 12:04:08 -0500

    autoplayworking

--- nytread/app/home/page.tsx
+++ nytread/app/home/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState, useEffect, useCallback } from "react";
+import { useState, useEffect, useCallback, useRef } from "react";
 import dynamic from "next/dynamic";
 
 const RecordAudio = dynamic(
@@ -11,6 +11,7 @@ const RecordAudio = dynamic(
 export default function HomePage() {
   const [audioUrl, setAudioUrl] = useState<string>("/NYTOpening.mp3");
   const [isRecording, setIsRecording] = useState<boolean>(false);
+  const audioRef = useRef<HTMLAudioElement>(null);
 
   const toggleRecording = useCallback(() => {
     setIsRecording(!isRecording);
@@ -30,11 +31,24 @@ export default function HomePage() {
     };
   }, [toggleRecording]);
 
+  useEffect(() => {
+    const playAudio = async () => {
+      if (audioRef.current) {
+        try {
+          await audioRef.current.play();
+        } catch (error) {
+          console.error("Error playing audio:", error);
+        }
+      }
+    };
+    playAudio();
+  }, [audioRef]);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-slate-500">
       <div className="flex flex-col items-center justify-center">
         <h1 className="text-4xl font-bold text-white mb-4">NYT Read</h1>
-        <audio src={audioUrl} controls />
+        <audio ref={audioRef} src={audioUrl} controls autoPlay />
         <RecordAudio
           direction="section"
           isRecording={isRecording}
